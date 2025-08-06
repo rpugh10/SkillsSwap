d547a4ad9809a60b6420d09a6072ce6c472ee747
Ryan Pugh <rpugh55@yahoo.com>
2025-08-05 23:40:04 -0700

    Merge branch 'main' of https://github.com/rpugh10/SkillsSwap

---
+++ src/test/java/com/example/skillsSwap/SkillsSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillsSwap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
