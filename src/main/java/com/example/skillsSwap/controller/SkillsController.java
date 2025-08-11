86696f00d4d65841ef932c2b5218a1adb827db68
Ryan Pugh <rpugh777@yahoo.com>
2025-08-10 20:00:33 -0700

    Added Crud methods from skills repo to skills service. Starting the skills controller

--- src/main/java/com/example/skillsSwap/controller/SkillsController.java
+++ src/main/java/com/example/skillsSwap/controller/SkillsController.java
@@ -1,5 +1,8 @@
 package com.example.skillsSwap.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SkillsController {
 
 }
--- src/main/java/com/example/skillsSwap/service/SkillsService.java
+++ src/main/java/com/example/skillsSwap/service/SkillsService.java
@@ -1,5 +1,41 @@
 package com.example.skillsSwap.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.skillsSwap.model.Skill;
+import com.example.skillsSwap.repository.SkillRepository;
+
+@Service
 public class SkillsService {
 
+    @Autowired
+    private SkillRepository repository;
+
+    public List<Skill> getAllSkills(){
+        return repository.findAll();
+    }
+
+    public Skill saveSkill(Skill skill){
+        return repository.save(skill);
+    }
+
+    public Optional<Skill> getSkillById(Long id){
+        return repository.findById(id);
+    }
+
+    public Skill updateSkill(Skill skill){
+        return repository.save(skill);
+    }
+
+    public Optional<Skill> deleteSkill(Long id){
+        return repository.findById(id);
+    }
+
+    public Optional<Skill> getSkillByUserId(Long userId){
+        return repository.findById(userId);
+    }
 }
