2f63d9e22a397728e8240809aba3a3fefc6e7d59
Ryan Pugh <rpugh777@yahoo.com>
2025-08-10 23:37:18 -0700

    Added get and post methods to the SkillsController

---
+++ src/main/java/com/example/skillsSwap/controller/Skills.java
@@ -0,0 +1,5 @@
+package com.example.skillsSwap.controller;
+
+public class Skills {
+
+}
--- src/main/java/com/example/skillsSwap/controller/SkillsController.java
+++ src/main/java/com/example/skillsSwap/controller/SkillsController.java
@@ -1,8 +1,40 @@
 package com.example.skillsSwap.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.skillsSwap.model.Skill;
+import com.example.skillsSwap.service.SkillsService;
 
 @RestController
 public class SkillsController {
+    
+    @Autowired
+    private SkillsService service;
 
+    @GetMapping("api/skill")
+    public List<Skill> getSkills(){
+        return service.getAllSkills();
+    }
+
+    @PostMapping("api/skill")
+    public Skill postSkill(@RequestBody Skill skill){
+        return service.saveSkill(skill);
+    }
+
+    @GetMapping("api/skill/{id}")
+    public ResponseEntity<Skill> getSkillById(@PathVariable Long id){
+        return service.getSkillById(id)
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
