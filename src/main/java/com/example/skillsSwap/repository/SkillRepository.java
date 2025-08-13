0f4b16ce1491112384f673998126e6594ab5da9e
Ryan Pugh <rpugh777@yahoo.com>
2025-08-12 20:08:02 -0700

    Added a custom method that returns a list of skills from a userId

--- src/main/java/com/example/skillsSwap/repository/SkillRepository.java
+++ src/main/java/com/example/skillsSwap/repository/SkillRepository.java
@@ -1,9 +1,12 @@
 package com.example.skillsSwap.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.skillsSwap.model.Skill;
 
 public interface SkillRepository extends JpaRepository<Skill, Long>{
 
+    List<Skill> findByUserId(Long userId);
 }
