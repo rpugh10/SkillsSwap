e560d009d7124bb0e528da23a784c3c69c2a417d
Ryan Pugh <rpugh777@yahoo.com>
2025-08-12 22:48:23 -0700

    Created Skill Request Controller

---
+++ src/main/java/com/example/skillsSwap/controller/SkillRequestController.java
@@ -0,0 +1,13 @@
+package com.example.skillsSwap.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.skillsSwap.service.SkillRequestService;
+
+@RestController
+public class SkillRequestController {
+
+    @Autowired
+    private SkillRequestService service;
+}
