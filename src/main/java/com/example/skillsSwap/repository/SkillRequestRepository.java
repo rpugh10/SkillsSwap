7001435fe17965c23182e8a1af64e16a16fc09e8
Ryan Pugh <rpugh777@yahoo.com>
2025-08-13 19:49:00 -0700

    Added a custom query to get skill requests by user id

--- src/main/java/com/example/skillsSwap/repository/SkillRequestRepository.java
+++ src/main/java/com/example/skillsSwap/repository/SkillRequestRepository.java
@@ -1,9 +1,11 @@
 package com.example.skillsSwap.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.skillsSwap.model.SkillRequest;
 
 public interface SkillRequestRepository extends JpaRepository<SkillRequest, Long>{
-
+    List<SkillRequest> getSkillRequestsByUserId(Long userId);
 }
