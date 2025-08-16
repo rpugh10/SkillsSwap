878f55d9b4dff4f96595e3848f3c80ced0cca584
Ryan Pugh <rpugh777@yahoo.com>
2025-08-15 23:50:02 -0700

    Created a Skill Request DTO

---
+++ src/main/java/com/example/skillsSwap/dto/SkillRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.skillsSwap.dto;
+
+public class SkillRequestDTO {
+
+    private Long id;
+    private String message;
+    private String status;
+    private Long userId;
+    private Long skillId;
+}
