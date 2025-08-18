e6ea31a00b2649965e57cd4ee4314b20cfa9e3cf
Ryan Pugh <rpugh777@yahoo.com>
2025-08-17 19:51:40 -0700

    Added DTO to entity method

--- src/main/java/com/example/skillsSwap/mapper/Mapper.java
+++ src/main/java/com/example/skillsSwap/mapper/Mapper.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import com.example.skillsSwap.dto.SkillDTO;
 import com.example.skillsSwap.dto.SkillRequestDTO;
 import com.example.skillsSwap.model.Skill;
+import com.example.skillsSwap.model.SkillRequest;
+import com.example.skillsSwap.model.User;
 
 @Component 
 public class Mapper {
@@ -20,8 +22,17 @@ public class Mapper {
         
     }
 
-    public SkillRequestDTO conRequestDTO(SkillRequestDTO skillRequestDTO){
-        SkillRequestDTO skillRequestDTO2 = modelMapper.map(skillRequestDTO, SkillRequestDTO.class);
-        return skillRequestDTO2;
+    public SkillRequestDTO conRequestDTO(SkillRequest skillRequest){
+        SkillRequestDTO dto = modelMapper.map(skillRequest, SkillRequestDTO.class);
+        dto.setUserId(skillRequest.getRequester().getId());
+        dto.setSkillId(skillRequest.getSkill().getId());
+        return dto;
+    }
+
+    public SkillRequest conRequestEntity(SkillRequestDTO dto, User user, Skill skill){
+        SkillRequest request = modelMapper.map(dto, SkillRequest.class);
+        request.setRequester(user);
+        request.setSkill(skill);
+        return request;
     }
 }
