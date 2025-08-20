d32ca1e45ca5731957e525f11bfa20f27b374664
Ryan Pugh <rpugh777@yahoo.com>
2025-08-19 23:39:49 -0700

    Created a userDTO class

---
+++ src/main/java/com/example/skillsSwap/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.example.skillsSwap.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String bio;
+}
