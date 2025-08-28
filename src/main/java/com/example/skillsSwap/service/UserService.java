32aba8749f90ef94f99a772baa89a978659a5b9f
Ryan Pugh <rpugh777@yahoo.com>
2025-08-27 23:32:43 -0500

    Updated methods to return userDTOS

--- src/main/java/com/example/skillsSwap/service/UserService.java
+++ src/main/java/com/example/skillsSwap/service/UserService.java
@@ -1,11 +1,13 @@
 package com.example.skillsSwap.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.skillsSwap.dto.UserDTO;
+import com.example.skillsSwap.exceptions.UserNotFoundException;
+import com.example.skillsSwap.mapper.Mapper;
 import com.example.skillsSwap.model.User;
 import com.example.skillsSwap.repository.UserRepository;
 
@@ -15,23 +17,43 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    @Autowired
+    private Mapper mapper;
+
+    public List<UserDTO> getAllUsers(){
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user ->  mapper.convertUserToDTO(user))
+                .toList();
     }
 
-    public User postUser(User user){
-        return userRepository.save(user);
+    public UserDTO postUser(UserDTO newUser){
+        User userEntity = mapper.convertUserDTOToEntity(newUser);
+        User savedUser = userRepository.save(userEntity);
+        return mapper.convertUserToDTO(savedUser);
     }
 
-    public Optional<User> getUserById(Long id){
-        return userRepository.findById(id);
+    public UserDTO getUserById(Long id){
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> new UserNotFoundException(id));
+        return mapper.convertUserToDTO(user);
     }
     
-    public User updateUser(User user){
-        return userRepository.save(user);
+    public UserDTO updateUser(Long id, UserDTO userDTO){
+        User existingUser = userRepository.findById(id)
+            .orElseThrow(() -> new UserNotFoundException(id));
+        
+            existingUser.setUsername(userDTO.getUsername());
+            existingUser.setEmail(userDTO.getEmail());
+            existingUser.setBio(userDTO.getBio());
+
+            User updatedUser = userRepository.save(existingUser);
+            return mapper.convertUserToDTO(updatedUser);
     }
 
     public void deleteUser(Long id){
+        userRepository.findById(id)
+            .orElseThrow(() -> new UserNotFoundException(id));
         userRepository.deleteById(id);
     }
 
