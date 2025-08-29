4fc2e61592870803ec143737f4a71b6e0dcfb748
Ryan Pugh <rpugh777@yahoo.com>
2025-08-29 11:24:48 -0500

    Updated method names

--- src/main/java/com/example/skillsSwap/mapper/Mapper.java
+++ src/main/java/com/example/skillsSwap/mapper/Mapper.java
@@ -1,8 +1,8 @@
 package com.example.skillsSwap.mapper;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 import com.example.skillsSwap.dto.SkillDTO;
 import com.example.skillsSwap.dto.SkillRequestDTO;
@@ -14,42 +14,45 @@ import com.example.skillsSwap.model.User;
 @Component 
 public class Mapper {
 
-    @Autowired
-    private ModelMapper modelMapper;
+   private final ModelMapper modelMapper;
+
+    public Mapper(ModelMapper modelMapper){
+        this.modelMapper = modelMapper;
+    }
 
 
-    public SkillDTO convertSkillToDTO(Skill skill){
+    public SkillDTO toSkillDTO(Skill skill){
         SkillDTO skillDTO = modelMapper.map(skill, SkillDTO.class);
         skillDTO.setUserId(skill.getUser().getId()); //Prevent sending the full User object
         return skillDTO;
     }
 
-    public Skill convertDTOToSkill(SkillDTO dto, User user){
+    public Skill toSkill(SkillDTO dto, User user){
         Skill skill = modelMapper.map(dto, Skill.class);
         skill.setUser(user);
         return skill;
     }
 
-    public SkillRequestDTO convertToDTO(SkillRequest skillRequest){
+    public SkillRequestDTO toSkillRequestDTO(SkillRequest skillRequest){
         SkillRequestDTO dto = modelMapper.map(skillRequest, SkillRequestDTO.class);
         dto.setUserId(skillRequest.getRequester().getId());
         dto.setSkillId(skillRequest.getSkill().getId());
         return dto;
     }
 
-    public SkillRequest convertToEntity(SkillRequestDTO dto, User user, Skill skill){
+    public SkillRequest toSkillRequest(SkillRequestDTO dto, User user, Skill skill){
         SkillRequest request = modelMapper.map(dto, SkillRequest.class);
         request.setRequester(user);
         request.setSkill(skill);
         return request;
     }
 
-    public UserDTO convertUserToDTO(User user){
+    public UserDTO toUserDTO(User user){
         UserDTO dto = modelMapper.map(user, UserDTO.class);
         return dto;
     }
 
-    public User convertUserDTOToEntity(UserDTO dto){
+    public User toUser (UserDTO dto){
         User user = modelMapper.map(dto, User.class);
         return user;
     }
