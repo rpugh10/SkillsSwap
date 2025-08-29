c12965b213f97ce8f4e7504dae0b0b8aa5bdd4ab
Ryan Pugh <rpugh777@yahoo.com>
2025-08-29 11:22:25 -0500

    Removed the email field

--- src/main/java/com/example/skillsSwap/dto/UserDTO.java
+++ src/main/java/com/example/skillsSwap/dto/UserDTO.java
@@ -1,6 +1,5 @@
 package com.example.skillsSwap.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -10,9 +9,7 @@ public class UserDTO {
     private Long id;
     @NotBlank(message = "Username cannot be empty")
     private String username;
-    @NotBlank(message = "email cannot be empty")
-    @Email(message = "Please provide a valid email address")
-    private String email;
+    
     @NotBlank(message = "bio cannot be empty")
     private String bio;
 }
