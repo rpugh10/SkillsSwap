7a1c50cd594799d3c5345deddc14ac46fa0c33a9
Ryan Pugh <rpugh777@yahoo.com>
2025-08-29 11:20:27 -0500

    Updated messages

--- src/main/java/com/example/skillsSwap/dto/SkillDTO.java
+++ src/main/java/com/example/skillsSwap/dto/SkillDTO.java
@@ -1,6 +1,8 @@
 package com.example.skillsSwap.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,9 +13,11 @@ public class SkillDTO {
     @NotBlank(message = "skill_name cannot be empty")
     private String skill_name;
     @NotBlank(message = "description cannot be empty")
-    @Size(max = 200, message = "description cannot exceed 200 character")
+    @Size(max = 200, message = "description cannot exceed 200 characters")
     private String description;
     @NotBlank(message = "category cannot be blank")
     private String category;
+    @NotNull(message = "userId is required")
+    @Positive(message = "userId must be a positive number")
     private Long userId;
 }
