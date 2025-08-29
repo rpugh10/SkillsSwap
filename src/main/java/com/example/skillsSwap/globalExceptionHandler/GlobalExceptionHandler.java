0448fa0910153bbcc9e22efcc4ffe28d112470e3
Ryan Pugh <rpugh777@yahoo.com>
2025-08-28 22:33:33 -0500

    Added a method to handle not valid exceptions

--- src/main/java/com/example/skillsSwap/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/skillsSwap/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.example.skillsSwap.globalExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.skillsSwap.apiError.ApiError;
@@ -57,6 +63,20 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+    MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        String fieldName = ((FieldError) error).getField();
+        String errorMessage = error.getDefaultMessage();
+        errors.put(fieldName, errorMessage);
+    });
+    return errors;
+}
+    
     
 
     
