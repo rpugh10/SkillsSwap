af859751d43fc49417de02a44d5c52abcca331fa
Ryan Pugh <rpugh777@yahoo.com>
2025-08-29 21:42:01 -0500

    removed verification for userId only using it for the mapper

--- src/main/java/com/example/skillsSwap/dto/SkillDTO.java
+++ src/main/java/com/example/skillsSwap/dto/SkillDTO.java
@@ -1,8 +1,6 @@
 package com.example.skillsSwap.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -17,7 +15,6 @@ public class SkillDTO {
     private String description;
     @NotBlank(message = "category cannot be blank")
     private String category;
-    @NotNull(message = "userId is required")
-    @Positive(message = "userId must be a positive number")
+
     private Long userId;
 }
