7a5ede944dbd425718ef44a40268552ab53e8c3a
Ryan Pugh <rpugh777@yahoo.com>
2025-09-01 14:59:18 -0700

    added user register method

--- src/main/java/com/example/skillsSwap/service/UserService.java
+++ src/main/java/com/example/skillsSwap/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.skillsSwap.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.skillsSwap.dto.UserDTO;
@@ -20,6 +21,17 @@ public class UserService {
     @Autowired
     private Mapper mapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User register(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if(user.getRole() == null){
+            user.setRole("ROLE_USER");
+        }
+        return userRepository.save(user);
+    }
+ 
     public List<UserDTO> getAllUsers(){
         List<User> users = userRepository.findAll();
         return users.stream()
