809f49eea24328abe4f3ed46fc7b7f6006a3510f
Ryan Pugh <rpugh777@yahoo.com>
2025-09-01 14:58:31 -0700

    Add find by username method

--- src/main/java/com/example/skillsSwap/repository/UserRepository.java
+++ src/main/java/com/example/skillsSwap/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.skillsSwap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.skillsSwap.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+    Optional<User> findByUsername(String username);
 }
